081b98734293b98728d221eee7f53a2c187e7e2d
Greif5 <sebastianxxl@msn.com>
2018-05-23 13:51:16 +0200

    Funktion zum packen eines Ordners

---
+++ tarfile.py
@@ -0,0 +1,8 @@
+#Source:
+#https://stackoverflow.com/questions/2032403/how-to-create-full-compressed-tar-file-using-python
+
+import tarfile
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
