9203acbb4bd490d6633a53896643d6669576393c
Greif5 <sebastianxxl@msn.com>
2018-08-21 16:10:32 +0200

    Settings Beispiel

---
+++ settings_example.py
@@ -0,0 +1,31 @@
+
+
+bot_id				= ''
+bot_debug			= ""
+bot_debug_id		=
+list_Admins			= []
+list_Ark			= []
+list_Avorion		= []
+list_Factorio		= []
+
+address				= "127.0.0.1"
+port				= "PORT"
+pw					= "PASSOWRD"
+
+steamCMD			= "/path/to/steamCMD"
+
+Avorion_Path		= "/path/to/AvorionServer"
+Avorion_BackupPath	= "/path/to/AvorionBackup"
+Avorion_BackupName	= "Avorion"
+Avorion_Launcher	= ""
+
+Ark_Path			= "/path/to/ArkServer"
+Ark_BackupPath		= "/path/to/ArkBackup"
+Ark_BackupName		= "Ark"
+Ark_Launcher		= "arkmanager"
+
+
+Factorio_Path		= "/path/to/FactorioServer"
+Factorio_BackupPath	= "/path/to/FactorioBackup"
+Factorio_BackupName	= "Factorio"
+Factorio_Launcher	= "/path/to/Factorio-init-script"
